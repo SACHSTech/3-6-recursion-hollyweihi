d8a702a8775f7fcc8e159a9fd84fa2af822df5e1
your_github_username <Xueyu.Wei25@ycdsbk12.ca>
2025-05-13 11:12:43 -0400

    Create printEven method for recursion

--- src/recursion/EvenNumbersInARange.java
+++ src/recursion/EvenNumbersInARange.java
@@ -1,5 +1,13 @@
 package recursion;
 
 public class EvenNumbersInARange {
+    public static void printEvens(int start, int end){
+        if(start > end){
+            return;
+        }
+        if(start %2 ==0){
+            System.out.println(start);
+        }
 
+    }
 }
