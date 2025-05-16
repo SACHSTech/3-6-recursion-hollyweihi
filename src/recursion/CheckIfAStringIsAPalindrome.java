833f7be250ebd013af58e23e7667ad3a0d01321d
your_github_username <Xueyu.Wei25@ycdsbk12.ca>
2025-05-16 19:55:23 -0400

    Add comment for CheckIfAStringIsAPalindrome

--- src/recursion/CheckIfAStringIsAPalindrome.java
+++ src/recursion/CheckIfAStringIsAPalindrome.java
@@ -1,18 +1,21 @@
 package recursion;
 
 public class CheckIfAStringIsAPalindrome {
-    public static boolean isPalindrome(String s){
-        if(s.length() <= 1){
+    public static boolean isPalindrome(String s) {
+
+        // Base Case
+        if (s.length() <= 1) {
             return true;
         }
-        if(s.charAt(0) != s.charAt(s.length()-1)){
+        if (s.charAt(0) != s.charAt(s.length() - 1)) {
             return false;
         }
 
+        // Recursive Case
         return isPalindrome(s.substring(1, (s.length() - 1)));
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(isPalindrome("HeooeH"));
         System.out.println(isPalindrome("adfkh"));
         System.out.println(isPalindrome(""));
