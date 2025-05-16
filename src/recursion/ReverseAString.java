fa54bc0abe47c1873156704b3d3fc2c8044263cc
your_github_username <Xueyu.Wei25@ycdsbk12.ca>
2025-05-16 19:54:26 -0400

    Add Comment for ReverseASrting

--- src/recursion/ReverseAString.java
+++ src/recursion/ReverseAString.java
@@ -1,14 +1,16 @@
 package recursion;
 
 public class ReverseAString {
-    public static String reverse(String s){
-        if(s.isEmpty()){
+    public static String reverse(String s) {
+        // Base Case
+        if (s.isEmpty()) {
             return s;
         }
+        // Recursive Case
         return reverse(s.substring(1)) + (s.charAt(0));
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(reverse("Hello"));
         System.out.println(reverse(""));
         System.out.println(reverse("Hello World!"));
